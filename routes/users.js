f1685d9ca4dd204086a5c8a6a16be38a1ba35b99
Goran Lisak <goran.lisak96@gmail.com>
2020-03-01 20:55:15 +0100

    Simplified users view

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 
 router.get('/', ensureLoggedIn('/'), (req, res) => {
-  res.render('users', req.user);
+  res.render('users', { user: req.user });
 });
 
 module.exports = router;
